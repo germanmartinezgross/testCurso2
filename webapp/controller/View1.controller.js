e86ec70514fed2fd786d05d2a9fc375cda271e8a
Ricardo Enrique Alonso <rialonso@proveedores.techint.com>
2024-02-02 22:01:37 +0000

    Pruebas01

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,6 +10,7 @@ sap.ui.define([
         return Controller.extend("project.controller.View1", {
             onInit: function () {
 
+                let a = 10;
             }
         });
     });
